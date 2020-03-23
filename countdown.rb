604193b8edc61d403767d5712ab996b34f397c1e
Arynn Boniface <arynn.boniface@gmail.com>
2020-03-23 21:19:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,6 @@
 def countdown(number)
   while number > 0 
     puts "#{number} SECOND(S)!"
-    sleep 1
     number -= 1
   end
     return "HAPPY NEW YEAR!"
@@ -11,4 +10,10 @@ end
 
 
 def countdown_with_sleep(number)
+    while number > 0 
+    puts "#{number} SECOND(S)!"
+    sleep 1
+    number -= 1
+  end
+    return "HAPPY NEW YEAR!"
 end
