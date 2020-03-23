3032998e3b4dd9c7ff5bfd8810e00372c6c2cd55
Arynn Boniface <arynn.boniface@gmail.com>
2020-03-23 21:15:19 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
+number = 15
 
-def countdown
+def countdown(number)
+  while number > 0 
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    return "HAPPY NEW YEAR!"
+  end
 end
